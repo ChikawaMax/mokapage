64e70cd74d745243fdae96f56fc141f87e852549
本間敬太 <keitakunv2@gmail.com>
2024-07-03 00:07:29 +0900

    🐛

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className="bg-muted flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
       <p className="text-xs text-muted-foreground">
-        &copy; 無断複写・転載を禁じます
+        &copy; 無断複写・転載を禁じます。
       </p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         {LinksArray.map((Links, index) => {
